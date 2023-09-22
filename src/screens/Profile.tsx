3ad02c05e0ae9166ec47c851a4c5167aa48d2472
Vitor Hugo <vishigo.vh@gmail.com>
2023-09-22 20:58:20 -0300

    alguns testes da tela profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -21,9 +21,6 @@ export default function Profile() {
 
   return (
     <>
-      <h2>
-        Profile
-      </h2>
       <p>
         Email:
         {' '}
--- src/tests/Profile.test.tsx
+++ src/tests/Profile.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, getByRole, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { ReactElement, JSXElementConstructor } from 'react';
 import App from '../App';
@@ -20,5 +20,44 @@ describe('Teste da tela Profile', () => {
     renderWithRouter(<Profile />);
     const pageTitle = screen.getByTestId('page-title');
     expect(pageTitle).toHaveTextContent('Profile');
+  });
+
+  test('testa se o botão Done Recipes leva para a rota certa', () => {
+    render(
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>,
+    );
+
+    const btnDoneRecipes = screen.getByRole('button', { name: /Done Recipes/i });
+    fireEvent.click(btnDoneRecipes);
+
+    expect(window.location.pathname).toBe('/done-recipes');
+  });
+
+  test('testa se o botão Favorites leva para a rota certa', () => {
+    render(
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>,
+    );
+
+    const btnFavorites = screen.getByRole('button', { name: /Favorite Recipes/i });
+    fireEvent.click(btnFavorites);
+
+    expect(window.location.pathname).toBe('/favorite-recipes');
+  });
+
+  test('testa se o botão logout leva para a rota certa e limpa o localStorage', () => {
+    render(
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>,
+    );
+
+    const btnLogout = screen.getByRole('button', { name: /Logout/i });
+    fireEvent.click(btnLogout);
+
+    expect(window.location.pathname).toBe('/');
   });
 });

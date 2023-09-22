062209e527f729b50e20df9ce76e7941b957b3d6
Vitor Hugo <vishigo.vh@gmail.com>
2023-09-22 19:23:54 -0300

    todos os elementos do profile (faltando a cobertura de testes)

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,13 +1,57 @@
 // Componente da tela principal de receitas de comidas;
+import { useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
-import Header from '../components/Header';
 
 export default function Profile() {
+  const navigate = useNavigate();
+  const email = localStorage.getItem('user');
+
+  const handleDoneRecipes = () => {
+    navigate('/done-recipes');
+  };
+
+  const handleFavoritesRecipes = () => {
+    navigate('/favorite-recipes');
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate('/');
+  };
+
   return (
     <>
-      <span>
-        teste
-      </span>
+      <h2>
+        Profile
+      </h2>
+      <p>
+        Email:
+        {' '}
+        <span data-testid="profile-email">
+          {email}
+          {' '}
+        </span>
+      </p>
+      <button
+        onClick={ handleDoneRecipes }
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      {' '}
+      <button
+        onClick={ handleFavoritesRecipes }
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      {' '}
+      <button
+        onClick={ handleLogout }
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <div>
         <Footer />
       </div>
